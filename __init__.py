104df39b0eca77eddad7eaca9f30538b36546b1f
daniel012 <oliveros.jesusd@gmail.com>
2021-08-17 16:01:24 -0500

    add complete files

--- __init__.py
+++ __init__.py
@@ -3,42 +3,50 @@ created: jesus oliveros
 
 """
 
+from datetime import datetime
 import pandas as pd
 import requests
 import sqlalchemy as sql
 from sqlalchemy.sql.expression import join
+from datetime import datetime
+import time 
 
-response  = requests.get('https://api.bitso.com/v3/trades/?book=btc_mxn')
-json_response = response.json()
-datos = json_response['payload']
-df = pd.DataFrame(datos)
+def extracBitsoapi():
+    response  = requests.get('https://api.bitso.com/v3/trades/?book=btc_mxn')
+    json_response = response.json()
+    datos = json_response['payload']
+    df = pd.DataFrame(datos)
 
-engine = sql.create_engine('mysql+pymysql://admin:1234689h0lA@palace.cluster-cnnnw4vopgyu.us-east-2.rds.amazonaws.com/palace')
+    engine = sql.create_engine('mysql+pymysql://admin:1234689h0lA@palace.cluster-cnnnw4vopgyu.us-east-2.rds.amazonaws.com/palace')
 
-initial_q = """INSERT  INTO bitso_trades
-(book, created_at , amount, maker_side, price, tid)
-VALUES
-"""
-values_q = ",".join(["""('{}','{}','{}','{}','{}','{}')""".format(
-    row.book, 
-    row.created_at,
-    row.amount, 
-    row.maker_side, 
-    row.price,
-    row.tid) for idx, row in df.iterrows()])
+    initial_q = """INSERT  INTO bitso_trades
+    (book, created_at , amount, maker_side, price, tid)
+    VALUES
+    """
+    values_q = ",".join(["""('{}','{}','{}','{}','{}','{}')""".format(
+        row.book, 
+        row.created_at,
+        row.amount, 
+        row.maker_side, 
+        row.price,
+        row.tid) for idx, row in df.iterrows()])
 
-end_q ="""
-    ON DUPLICATE KEY UPDATE 
-        book = values(book),
-        created_at = values(created_at) ,
-        amount = values(amount) ,
-        maker_side = values(maker_side), 
-        price = values(price) ,
-        tid = values(tid);
-"""
+    end_q ="""
+        ON DUPLICATE KEY UPDATE 
+            book = values(book),
+            created_at = values(created_at) ,
+            amount = values(amount) ,
+            maker_side = values(maker_side), 
+            price = values(price) ,
+            tid = values(tid);
+    """
 
-query = initial_q + values_q + end_q
+    query = initial_q + values_q + end_q
 
-engine.execute(query)
+    engine.execute(query)
+    return
 
-# print(query)
+while True:
+   extracBitsoapi()
+   print('act BD a  las {} con'.format(datetime.now()))
+   time.sleep(15)
